d26b00daf3e7a6d5e53e2442d69b2e96a5fe4499
Jan Kocet <jan.kocet@student.um.si>
2024-06-12 15:05:20 +0200

    .

--- sim_list.py
+++ sim_list.py
@@ -3,7 +3,7 @@ import subprocess
 
 
 sumo_path = r"C:\Program Files (x86)\Eclipse\Sumo\bin\sumo-gui.exe"
-sumo_config = "sim_list.sumocfg"
+sumo_config = "sim_list.sumo.cfg"
 
 sumo_cmd = [sumo_path, "-c", sumo_config, "--start"]
 
